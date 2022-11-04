48257ca2310f7684a5aa0882f55eee8a595ea2c6
ilara90 <miss_vesna08@mail.ru>
2022-11-04 17:46:15 +0300

    Initial commit

---
+++ WBGettingData/WBGettingData/Model.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WBGettingData;
+
+public class Model
+{
+    [JsonPropertyName("state")]
+    public int State { get; set; }
+
+    [JsonPropertyName("version")]
+    public int Version { get; set; }
+
+    [JsonPropertyName("data")]
+    public Data Data { get; set; }
+}
+
+public class Product
+{
+    [JsonPropertyName("brand")]
+    public string Brand { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+}
+
+public class Data
+{
+    [JsonPropertyName("products")]
+    public Product[] Products { get; set; }
+}
---
+++ WBGettingData/WBGettingData/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Text.Json;
+using System.Web;
+using Microsoft.Office.Interop.Excel;
+using _excel = Microsoft.Office.Interop.Excel;
+using Model = WBGettingData.Model;
+
+string[] keys = new[] { "Игрушки", "Настолки" };
+
+_Application excel = new _excel.Application();
+Workbook workbook = excel.Workbooks.Add();
+
+foreach (var key in keys)
+{
+    HttpClient client = new HttpClient();
+    var urlDecode = HttpUtility.UrlDecode(
+        $"https://search.wb.ru/exactmatch/ru/common/v4/search?dest=-1029256,-102269,-2162196,-1257786&query={key}&resultset=catalog&sort=popular");
+    var response = await client.GetAsync(urlDecode);
+    var result = await response.Content.ReadAsStringAsync();
+    var deserializedResult = JsonSerializer.Deserialize<Model>(result);
+
+    var worksheet = (Worksheet)workbook.Worksheets[Array.IndexOf(keys, key) + 1];
+    worksheet.Name = key;
+    worksheet.Cells[1, "A"] = "Title";
+    foreach (var product in deserializedResult.Data.Products)
+    {
+        var index = Array.IndexOf(deserializedResult.Data.Products, product) + 2;
+        worksheet.Cells[index, "A"] = product.Name;
+    }
+    
+}
+
+workbook.SaveAs("C:\\example_workbook.xlsx");
